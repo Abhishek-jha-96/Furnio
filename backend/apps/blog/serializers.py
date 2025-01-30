0f989d592870bfaa3af9d8c7d96403f958d9bbb5
Abhishek Jha <abhishekjha96500@gmail.com>
2025-01-30 23:08:34 +0530

    - Ensures content ownership and platform security.

--- backend/apps/blog/serializers.py
+++ backend/apps/blog/serializers.py
@@ -6,7 +6,15 @@ from apps.blog.models import Blog
 class BlogSerializer(ModelSerializer):
     class Meta:
         model = Blog
-        fields = ("id", "title", "content", "image", "author", "created_ts", "modified_ts")
+        fields = (
+            "id",
+            "title",
+            "content",
+            "image",
+            "author",
+            "created_ts",
+            "modified_ts",
+        )
         read_only_fields = ("id", "created_ts", "modified_ts", "author")
 
     image = URLField(required=False, allow_blank=True)
--- backend/apps/blog/views.py
+++ backend/apps/blog/views.py
@@ -1,8 +1,10 @@
 from apps.core.views import BaseViewset
 from apps.blog.models import Blog
+from apps.blog.serializers import BlogSerializer
+from apps.blog.permission import BlogPermission
 
 
 class BlogViewset(BaseViewset):
-    permission_classes = [None]
+    permission_classes = BlogPermission
     serializer_class = BlogSerializer
     queryset = Blog.objects.all()
