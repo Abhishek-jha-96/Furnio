4da9d27740dd30668b1222eb90e47a51045876f4
Abhishek Jha <abhishekjha96500@gmail.com>
2025-03-01 18:17:00 +0530

    added cart table and total and subtotal calculation

--- frontend/src/app/store/cart/(components)/Row.tsx
+++ frontend/src/app/store/cart/(components)/Row.tsx
@@ -4,22 +4,18 @@ export default function Row({
   Product,
   Price,
   Quantity,
-  Subtotal,
 }: ProductTableProps) {
   return (
-    <div className="grid grid-cols-5 grid-rows-1 gap-8 px-16 py-4">
+    <div className="w-full grid grid-cols-3 grid-rows-1 gap-16 px-2 py-4">
       {/* handle the grid rows */}
-      <div className="col-span-2">
+      <div className="">
         <h3>{Product}</h3>
       </div>
-      <div className="col-start-3">
+      <div className="col-start-2">
         <h3>{Price}</h3>
       </div>
-      <div className="col-start-4">
+      <div className="col-start-3">
         <h3>{Quantity}</h3>
-      </div>
-      <div className="col-start-5">
-        <h3>{Subtotal}</h3>
       </div>
     </div>
   );
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { ICartData } from '@/api/cart/contants';
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -11,3 +12,23 @@ export const formatDateTime = (timestamp: string) => {
     timeStyle: "short",
   }).format(new Date(timestamp));
 };
+
+export const getSubTotal = (data: ICartData[]) => {
+  let sub_total = 0;
+  data.forEach(element => {
+    sub_total += (element.product_details.price * element.product_details.price);
+  });
+
+  return Math.fround(sub_total);
+}
+
+export const getTotal = (data: ICartData[]) => {
+  let total = 0;
+  data.forEach(element => {
+    total += (element.product_details.price * element.product_details.price);
+  });
+
+  let discount = total / 10; 
+
+  return Math.fround(total - discount);
+}
